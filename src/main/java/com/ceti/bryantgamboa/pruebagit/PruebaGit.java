4407731be63da58ac426cd854874fe54ae6b27d9
Bryant <bryantgamboajimenez@gmail.com>
2022-05-03 20:07:22 -0500

    Iniciando Proyecto

---
+++ src/main/java/com/ceti/bryantgamboa/pruebagit/PruebaGit.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.ceti.bryantgamboa.pruebagit;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Rumyn
+ */
+public class PruebaGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Utilidades.Mensaje("Hola amigo");
+        Utilidades.Mensaje("Como stas?");
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/main/java/com/ceti/bryantgamboa/pruebagit/Utilidades.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ceti.bryantgamboa.pruebagit;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Rumyn
+ */
+public class Utilidades {
+    public static void Mensaje(Object mensaje){
+        JOptionPane.showMessageDialog(null, mensaje);
+    }
+}
