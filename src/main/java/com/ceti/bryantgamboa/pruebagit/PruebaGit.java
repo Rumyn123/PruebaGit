7c4e661a53860cc236a112a5b95b9608a3c424a5
Bryant <bryantgamboajimenez@gmail.com>
2022-05-03 19:55:46 -0500

    Se añadio mensaje de HolaAmigo

---
+++ src/main/java/com/ceti/bryantgamboa/pruebagit/PruebaGit.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.ceti.bryantgamboa.pruebagit;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Rumyn
+ */
+public class PruebaGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hola amigo");
+        // TODO code application logic here
+    }
+    
+}
